db6be518e15ee294c0e1bd1f980dce17b323b27d
guspowell <augustuspowell@me.com>
2015-01-20 12:28:51 +0000

    all tests passed and refactored methods

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -1,17 +1,9 @@
 var FizzBuzz = function() {};
 
-FizzBuzz.prototype.isDivisibleByThree = function(number) {
-<<<<<<< HEAD
-	return (number % 3 === 0);
-=======
-	return number % 3 === 0;
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+	return number % divisor === 0;
 };
 
-FizzBuzz.prototype.isDivisibleByFive = function(number) {
-	return number % 5 === 0;
-};
-
-FizzBuzz.prototype.isDivisibleByThreeAndFive = function(number) {
-	return number % 3 === 0 && number % 5 === 0;
->>>>>>> a30457ffde928228348308fd7905fabe11cdde7a
+FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+	return this._isDivisibleBy(number, 15);
 };
--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -2,47 +2,45 @@ describe("FizzBuzz", function() {
 
 	var fizzbuzz 
 
+	beforeEach(function() {
+		fizzbuzz = new FizzBuzz();
+	});
+
 	describe("knows when a number", function() {
 
 		it("is divisible by 3", function() {
 
-			fizzbuzz = new FizzBuzz();
-			expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+			expect(fizzbuzz._isDivisibleBy(3, 3)).toBe(true);
 
 		});
 
 		it("is not divisible by 3", function() {
 
-			fizzbuzz = new FizzBuzz();
-			expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+			expect(fizzbuzz._isDivisibleBy(1,3)).toBe(false);
 
 		});
 
 		it("is divisible by 5", function() {
 
-			fizzbuzz = new FizzBuzz();
-			expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+			expect(fizzbuzz._isDivisibleBy(5, 5)).toBe(true);
 
 		});
 
 		it("is NOT divisible by 5", function() {
 
-			fizzbuzz = new FizzBuzz();
-			expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+			expect(fizzbuzz._isDivisibleBy(1,5)).toBe(false);
 
 		});
 
 		it("is divisible by 3 and 5", function() {
 
-			fizzbuzz = new FizzBuzz();
-			expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+			expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
 
 		});
 
 		it("is NOT divisible by 3 and 5", function() {
 
-			fizzbuzz = new FizzBuzz();
-			expect(fizzbuzz.isDivisibleByThreeAndFive(1)).toBe(false);
+			expect(fizzbuzz.isDivisibleByFifteen(1)).toBe(false);
 
 		});
 
