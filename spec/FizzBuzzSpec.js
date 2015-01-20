d831866b9601a7437bf090cb522abbed8d9de5a7
ciawalsh <cia.walsh@gmail.com>
2015-01-20 12:01:15 +0000

    First Commit

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,39 @@
+describe("FizzBuzz", function() {
+
+	var fizzbuzz 
+
+	describe("knows when a number", function() {
+
+		it("is divisible by 3", function() {
+
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+
+		});
+
+		it("is NOT divisible by 3", function() {
+
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+
+		});
+
+		it("is divisible by 5", function() {
+
+		});
+
+		it("is NOT divisible by 5", function() {
+
+		});
+
+		it("is divisible by 3 and 5", function() {
+
+		});
+
+		it("is NOT divisible by 3 and 5", function() {
+
+		});
+
+	});
+
+});
