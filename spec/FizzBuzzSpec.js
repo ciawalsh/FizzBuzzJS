635452427c6e377ea942afd7b276f857ef3c7183
guspowell <augustuspowell@me.com>
2015-01-20 11:24:39 +0000

    first commit

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,19 @@
+describe("fizzbuzz", function() {
+
+	var fizzbuzz;
+
+	describe("knows when a number", function() {
+
+		it("is divisible by 3", function() {
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+		});
+
+		it("is not divisible by 3", function() {
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+		});
+
+	});
+
+});
